aaeba9ac61829d26a3bc4fa91419dcad2ff9d737
@xcellee0 <160401161+derayareychele@users.noreply.github.com>
2025-05-16 21:17:58 +0800

    Update test_voter_checker.py

--- test_voter_checker.py
+++ test_voter_checker.py
@@ -8,7 +8,7 @@ class TestVoterEligibility(unittest.TestCase):
 
     def test_eligible_voter(self):
         self.assertTrue(is_eligible_to_vote(20, True))
-        
+
     def test_underage_voter(self):
         self.assertFalse(is_eligible_to_vote(16, False))
 
@@ -20,7 +20,7 @@ class TestVoterEligibility(unittest.TestCase):
 
     def test_exactly_18_and_non_citizen(self):
         self.assertTrue(is_eligible_to_vote(18, True))
-        
+
     def test_negative_age(self):
         self.assert(is_eligible_to_vote(-0, False))
 
