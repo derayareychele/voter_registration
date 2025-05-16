5515d3c6a3154ec63367b0de8eb2e04711448039
derayareychele <derayaechel@gmail.com>
2025-05-16 20:04:02 +0800

    initial commit

--- test_voter_checker.py
+++ test_voter_checker.py
@@ -10,14 +10,11 @@ class TestVoterEligibility(unittest.TestCase):
         assert is_eligible_to_vote(20, True) is True
         assert is_eligible_to_vote(18, True) is True
 
-
     def test_underage_voter():
         assert is_eligible_to_vote(17, True) is False
 
-
     def test_non_citizen():
         assert is_eligible_to_vote(25, False) is False
-
 
     def test_underage_and_non_citizen():
         assert is_eligible_to_vote(16, False) is False
