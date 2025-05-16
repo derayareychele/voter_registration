bb4c1a1dceec3980bfed1789c2cf88fda328e6d1
@xcellee0 <160401161+derayareychele@users.noreply.github.com>
2025-05-16 21:11:32 +0800

    Update test_voter_checker.py

--- test_voter_checker.py
+++ test_voter_checker.py
@@ -6,24 +6,23 @@ class TestVoterEligibility(unittest.TestCase):
     def test_valid_voter(self):
         self.assertTrue(is_eligible_to_vote(18, True))
 
-    def test_eligible_voter():
-        assert is_eligible_to_vote(20, True) is True
-        assert is_eligible_to_vote(18, True) is True
+    def test_eligible_voter(self):
+        self.assert is_eligible_to_vote(20, True) is True
+        
+    def test_underage_voter(self):
+        self.assert is_eligible_to_vote(16, False) is False
 
-    def test_underage_voter():
-        assert is_eligible_to_vote(16, False) is False
+    def test_non_citizen(self):
+        self.assert is_eligible_to_vote(25, False) is False
 
-    def test_non_citizen():
-        assert is_eligible_to_vote(25, False) is False
+    def test_underage_and_non_citizen(self):
+        self.assert is_eligible_to_vote(16, False) is False
 
-    def test_underage_and_non_citizen():
-        assert is_eligible_to_vote(16, False) is False
+    def test_exactly_18_and_non_citizen(self):
+        self.assert is_eligible_to_vote(18, True) is True
 
-    def test_exactly_18_and_non_citizen():
-        assert is_eligible_to_vote(18, True) is True
-
-    def test_negative_age():
-        assert is_eligible_to_vote(-0, False) is False
+    def test_negative_age(self):
+        self.assert is_eligible_to_vote(-0, False) is False
 
 if __name__ == "__ main __":
     unittest.main
