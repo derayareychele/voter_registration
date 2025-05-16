57ddd654c0077100b22dc7f3775e247301ad8bc5
@xcellee0 <160401161+derayareychele@users.noreply.github.com>
2025-05-16 21:15:52 +0800

    Update test_voter_checker.py

--- test_voter_checker.py
+++ test_voter_checker.py
@@ -7,22 +7,22 @@ class TestVoterEligibility(unittest.TestCase):
         self.assertTrue(is_eligible_to_vote(18, True))
 
     def test_eligible_voter(self):
-        self.assertTrue (is_eligible_to_vote(20, True))
+        self.assertTrue(is_eligible_to_vote(20, True))
         
     def test_underage_voter(self):
-        self.assertFalse (is_eligible_to_vote(16, False))
+        self.assertFalse(is_eligible_to_vote(16, False))
 
     def test_non_citizen(self):
-        self.assertFalse (is_eligible_to_vote(25, False))
+        self.assertFalse(is_eligible_to_vote(25, False))
 
     def test_underage_and_non_citizen(self):
-        self.assertFalse (is_eligible_to_vote(16, False))
+        self.assertFalse(is_eligible_to_vote(16, False))
 
     def test_exactly_18_and_non_citizen(self):
-        self.assertTrue (is_eligible_to_vote(18, True))
+        self.assertTrue(is_eligible_to_vote(18, True))
         
     def test_negative_age(self):
-        self.assert (is_eligible_to_vote(-0, False))
+        self.assert(is_eligible_to_vote(-0, False))
 
 
 if __name__ == "__ main __":
